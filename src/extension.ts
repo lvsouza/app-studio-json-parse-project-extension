3497b104b4e749355af89f6f9e11664d43ba2657
Lucas Souza <ls18017@gmail.com>
2024-12-30 08:16:46 -0300

    feat: add parser

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,22 @@
-import { ExtensionBase, IPlatformAction } from '@pb/extension-basics';
+import { ExtensionBase, TParser, } from '@pb/extension-basics';
 
 
 new class ProjectExport extends ExtensionBase {
-
-  public platformActions: IPlatformAction[] = [
+  parsers: TParser[] = [
     {
-      key: 'download-as-json',
-      action: () => {
-        console.log('EXTENSION: Download as JSON executed');
-
-        this.application.downloadFile('fileName', 'fileType', 'fileContent')
+      key: 'project-to-json',
+      parser(project) {
+        return JSON.stringify(project);
       },
     }
   ];
 
 
-  public activate() {
+  activate() {
     console.log('EXTENSION: Activating');
   }
 
-  public deactivate() {
+  deactivate() {
     console.log('EXTENSION: Deactivating');
   }
 };
