c1f69ebbd858821dc0d6de66009fb5289994026b
Lucas Souza <ls18017@gmail.com>
2025-06-12 13:22:21 -0300

    refactor: update parsers and update extension base

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,29 @@
-import { ExtensionBase, TParser } from '@pb/extension-basics';
+import { ExtensionBase, Parser } from '@pb/extension-basics';
 
 
 new class ProjectExport extends ExtensionBase {
-  parsers: TParser[] = [
-    {
-      key: 'project-to-json',
-      parser: async () => {
-        this.application.commands.editor.feedback(`Download initialized...`, 'success');
-        return JSON.stringify(
-          this.application.dataProviders.project()
-        );
-      },
-    }
-  ];
+  jsonParser = new Parser({
+    key: 'project-to-json',
+    parser: async () => {
+      this.application.commands.editor.feedback(`Download initialized...`, 'success');
+
+      const project = await this.application.dataProviders.project();
+
+      return {
+        name: project.name,
+        content: JSON.stringify(project)
+      };
+    },
+  });
 
 
   async activate() {
     console.log('EXTENSION: Activating');
+    this.application.parsers.register(this.jsonParser);
   }
 
   async deactivate() {
     console.log('EXTENSION: Deactivating');
+    this.application.parsers.unregister(this.jsonParser);
   }
 };
